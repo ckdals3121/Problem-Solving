94e06697905ec00cf4762e9b69e59f3f8687b04e
Desmos <42434324+ckdals3121@users.noreply.github.com>
2024-03-11 19:45:47 +0900

    [level 2] Title: 올바른 괄호, Time: 12.52 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = True
+    stack = []
+    
+    for i in range(len(s)) :
+        if s[i] == '(' :
+            stack.append(s[i])
+        else :
+            if len(stack) == 0 :
+                return False
+            else :
+                stack.pop()
+    
+    if len(stack) == 0 :
+        return True
+    else :
+        return False
